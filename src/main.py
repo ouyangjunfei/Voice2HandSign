0c8f3b42f82cd1a1cbeaefd93f0f816726289472
ouyangjunfei <1342597034@qq.com>
2019-06-02 22:12:53 +0800

    Update

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import sys
 import os
 
 if __name__ == '__main__':
-    voice_path = os.path.abspath('..') + "/resources/test.wav"
+    voice_path = os.path.abspath('..') + "/resources/sample.wav"
     image_path = os.path.abspath('..') + "/resources/images/"
 
     app = QtWidgets.QApplication(sys.argv)
